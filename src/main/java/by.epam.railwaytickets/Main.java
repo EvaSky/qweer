29882f365ef5d855f1cb4f51e38029631690aa32
Volha Shakhrai <volha_shakhrai@epam.com>
2016-12-08 15:16:32 +0300

    f111

--- src/main/java/by.epam.railwaytickets/Main.java
+++ src/main/java/by.epam.railwaytickets/Main.java
@@ -6,5 +6,6 @@ package by.epam.railwaytickets;
 public class Main {
     public static void main(String[] args) {
         System.out.println("In main");
+        System.out.println("In main111");
     }
 }
