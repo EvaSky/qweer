8695698eac92c0e0f5a71f771c3ab0b24b555b65
Volha Shakhrai <volha_shakhrai@epam.com>
2016-12-08 11:23:30 +0300

    add country dao test

--- src/main/java/by.epam.railwaytickets/dao/impl/CountryDAOImpl.java
+++ src/main/java/by.epam.railwaytickets/dao/impl/CountryDAOImpl.java
@@ -1,18 +1,18 @@
 package by.epam.railwaytickets.dao.impl;
 
-import by.epam.railwaytickets.dao.ICountryDAO;
-import by.epam.railwaytickets.model.Country;
+
+
 
 /**
  * Created by Volha_Shakhrai on 12/7/2016.
  */
-public class CountryDAOImpl implements ICountryDAO {
-    @Override
-    public void save(Country country) {
+public class CountryDAOImpl {
+
+    public void save() {
 
     }
 
-    @Override
+
     public boolean delete(int id) {
         return false;
     }
---
+++ src/test/java/by/epam/railwaytickets/dao/impl/CountryDAOImplTest.java
@@ -0,0 +1,11 @@
+package by.epam.railwaytickets.dao.impl;
+
+
+
+/**
+ * Created by Volha_Shakhrai on 12/8/2016.
+ */
+public class CountryDAOImplTest {
+
+
+}
